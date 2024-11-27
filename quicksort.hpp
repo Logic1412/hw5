7db836678b6959d323ad2638661a4d879d831fbc
Nicholas Weaver <ncweaver@ucdavis.edu>
2024-11-27 02:05:43 +0000

    Forgot to call by reference.

--- quicksort.hpp
+++ quicksort.hpp
@@ -8,7 +8,7 @@
 // So we don't need a separate function we have start and end set to INT_MIN
 // for determining we need to start the sorting.
 template <class T>
-inline void quicksort(std::vector<T> vin, int start=INT_MIN, int end=INT_MIN){
+inline void quicksort(std::vector<T> &vin, int start=INT_MIN, int end=INT_MIN){
     (void) vin;
     (void) start;
     (void) end;
