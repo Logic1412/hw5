33b35031c80af2b1259e506e79ce3ea1c3952e3d
Shangxiao Xie <shaxie@ucdavis.edu>
2024-12-04 18:33:49 -0800

    Update quicksort.hpp

--- quicksort.hpp
+++ quicksort.hpp
@@ -4,16 +4,32 @@
 #include <vector>
 #include <climits>
 
-
 // So we don't need a separate function we have start and end set to INT_MIN
 // for determining we need to start the sorting.
 template <class T>
-inline void quicksort(std::vector<T> &vin, int start=INT_MIN, int end=INT_MIN){
+inline void quicksort(std::vector<T> &vin, int start = INT_MIN, int end = INT_MIN)
+{
     (void) vin;
     (void) start;
     (void) end;
-    std::cout << "IMPLEMENT THIS!\n"
-}
 
+    if (start == INT_MIN) start = 0;
+    if (end == INT_MIN) end = vin.size() - 1;
+    if (start >= end) return;
+
+    T Piv = vin[end];
+    int i = start;
+    for (int j = start; j < end; ++j)
+    {
+        if (vin[j] <= Piv)
+        {
+            std::swap(vin[i], vin[j]);
+            i++;
+        }
+    }
+    std::swap(vin[i], vin[end]);
+    quicksort(vin, start, i - 1);
+    quicksort(vin, i + 1, end);
+}
 
 #endif
