be796a576344f2209112e2236dc03adc64b51cbe
Shangxiao Xie <shaxie@ucdavis.edu>
2024-12-04 18:35:41 -0800

    Update primes_test.cpp

--- primes_test.cpp
+++ primes_test.cpp
@@ -2,6 +2,7 @@
 
 #include "primes.hpp"
 #include <ranges>
+#include <chrono>
 
 TEST(PrimesTest, TestBasic)
 {
@@ -9,7 +10,6 @@ TEST(PrimesTest, TestBasic)
     auto index = 0;
     for (auto i : primerange() | std::views::take(50000))
     {
-        std::cout << i << "\n";
         if (i < 30)
         {
             EXPECT_EQ(i, testprimes[index++]);
@@ -19,5 +19,28 @@ TEST(PrimesTest, TestBasic)
     {
         std::cout << i << "\n";
     }
+
+}
+
+TEST(PrimesTest, TestMemoization){
+    long long testprimes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    auto index = 0;
+    for (auto i : primerange() | std::views::take(500000))
+    {
+        if (i < 30)
+        {
+            EXPECT_EQ(i, testprimes[index++]);
+        }
+    }
+    auto second = std::chrono::system_clock::now();
+
+    for (auto i : primerange() | std::views::drop(499999) | std::views::take(1))
+    {
+        std::cout << i << "\n";
+        ASSERT_EQ(i, 7368787);
+    }
+     auto third = std::chrono::system_clock::now();
+     auto t1 = std::chrono::duration_cast<std::chrono::microseconds>(third - second).count();
+     ASSERT_TRUE(t1 < 10000);
 
 }
